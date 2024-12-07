32ebd56c968b8e97a36cac069e118378412c3d2c
Pavel Olyšar <olysarp@gmail.com>
2024-12-07 08:37:21 +0100

    add hono and bun run dev script

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => c.text('Hello, World!'));
+
+Bun.serve({
+  fetch(req) {
+    return app.fetch(req);
+  },
+  port: 6969,
+});
+
+console.log('Server running on http://localhost:6969');
